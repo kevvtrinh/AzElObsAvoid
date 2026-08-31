36b53fcd4a3964f23768ae8b7ebdda2e8607d0bc
Kevin Trinh <kevvtrinh@users.noreply.github.com>
2026-08-30 22:35:42 -0600

    Add gated BMTP plane reuse

--- tests/testPlannerOptions.m
+++ tests/testPlannerOptions.m
@@ -26,6 +26,9 @@ verifyFalse(testCase, options.EnableStagnationStop);
 verifyEqual(testCase, options.StagnationIterationLimit, 5);
 verifyEqual(testCase, options.StagnationImprovementTolerance_s, ...
     options.ArrivalTimeTolerance_s);
+verifyFalse(testCase, options.EnablePlaneReuse);
+verifyEqual(testCase, options.PlaneReuseImprovementTolerance_s, ...
+    options.ArrivalTimeTolerance_s);
 end
 
 function testPartialOverridesResolveAndNormalize(testCase)
@@ -39,6 +42,7 @@ overrides = struct( ...
     "MaximumWaitRefinementIterations", 8, ...
     "EnableStagnationStop", 1, ...
     "StagnationIterationLimit", 8, ...
+    "EnablePlaneReuse", 1, ...
     "ArrivalTimeTolerance_s", 2e-3, ...
     "Verbose", 1);
 options = obstacleAvoidance.input.resolvePlannerOptions(overrides);
@@ -64,6 +68,8 @@ verifyEqual(testCase, options.MaximumWaitRefinementIterations, 8);
 verifyTrue(testCase, options.EnableStagnationStop);
 verifyEqual(testCase, options.StagnationIterationLimit, 8);
 verifyEqual(testCase, options.StagnationImprovementTolerance_s, 2e-3);
+verifyTrue(testCase, options.EnablePlaneReuse);
+verifyEqual(testCase, options.PlaneReuseImprovementTolerance_s, 2e-3);
 verifyTrue(testCase, options.Verbose);
 end
 
